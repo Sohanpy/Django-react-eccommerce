918cc07b6f7fde2645114cda3cab697f453c0722
Sohanpy <sohanmock@gmail.com>
2019-10-01 15:26:38 +0600

    Initial Commit

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/34901FB6901F7E14/Django React/Eccommerce/Project_titan/backend/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
